60d7bc93b91eeb7dda1253c7a185ec6bb450f53a
Ivanovic <111422951+NoNameVI@users.noreply.github.com>
2025-11-10 14:13:58 +0700

    Add files via upload

--- ProjectVinMall/src/projectvinmall/Book.java
+++ ProjectVinMall/src/projectvinmall/Book.java
@@ -33,28 +33,3 @@ public class Book extends Product{
         
 }
 
-class Drink extends Product{
-    protected String size;
-
-    public Drink(String id, String name, double price, double rating, String size) {
-        super(id, name, price, rating);
-        this.size = size;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    @Override
-    public String toString() {
-        return getId() + "-" +
-           getName() + "-" +
-           String.format("%.0f-%.1f-", getPrice(), getRating()) +
-           getSize();
-    }
-    
-}
